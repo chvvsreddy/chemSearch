02160017ea15aa8505c8141aa543369294538dac
chvvs reddy <reddy.chvvs@gmail.com>
2025-06-18 16:13:47 +0530

    next image config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'pubchem.ncbi.nlm.nih.gov',
+        pathname: '/image/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
