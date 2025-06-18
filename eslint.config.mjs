91f8951a5c0c8eb53b761e388fad67a9ff8e5999
chvvs reddy <reddy.chvvs@gmail.com>
2025-06-18 16:26:00 +0530

    error

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -28,6 +28,20 @@ const eslintConfig = [
 //     "no-unused-vars": "off"
 //   }
 // }
+
+{
+  "extends": ["next/core-web-vitals"],
+  "rules": {
+    "@typescript-eslint/no-unused-vars": [
+      "warn",
+      { 
+        "argsIgnorePattern": "^_",
+        "varsIgnorePattern": "^_",
+        "caughtErrorsIgnorePattern": "^_"
+      }
+    ]
+  }
+}
 ];
 
 export default eslintConfig;
